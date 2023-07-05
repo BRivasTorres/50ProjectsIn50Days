765e15c48b45128ace97bf74b92342fd1faf113f
Billy Torres <btorresr75@gmail.com>
2023-07-04 18:14:32 -0600

    Proyecto 1 finalizado

--- Day1/index.js
+++ Day1/index.js
@@ -1,8 +1,9 @@
 let currImage = document.querySelectorAll(".picture")
+let titleImage = document.querySelectorAll(".title")
 
 currImage.forEach(img => {
     img.addEventListener("click", () => {
-        currImage.forEach(img => {
+        currImage.forEach((img) => {
             img.classList.remove("curr")
         })
         img.classList.add("curr")
