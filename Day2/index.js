5616297d3839367fd789ac24be759e649217e1a0
Billy Torres <btorresr75@gmail.com>
2023-07-04 18:54:43 -0600

    Inicializando proyecto 2

---
+++ Day2/index.js
@@ -0,0 +1,10 @@
+const nextBtn = document.getElementsById("next")
+const prevBtn = document.getElementsById("prev")
+
+nextBtn.addEventListener("click", () => {
+    console.log("next")
+})
+
+prevBtn.addEventListener("click", () => {
+    console.log("prev")
+})
