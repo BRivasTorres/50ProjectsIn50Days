ac69263a7396fa2e80c8b6781b87c045e155f7dc
Billy Torres <btorresr75@gmail.com>
2023-07-08 17:38:38 -0600

    Finalizando estilos

--- Day2/index.js
+++ Day2/index.js
@@ -1,8 +1,10 @@
 const nextBtn = document.getElementById("next")
 const prevBtn = document.getElementById("prev")
+const btns = document.querySelectorAll(".btn")
 
 nextBtn.addEventListener("click", () => {
     console.log("next")
+    console.log(btns)
 })
 
 prevBtn.addEventListener("click", () => {
