be55b54026729340935390e14f0e99b9180a8929
Billy Torres <btorresr75@gmail.com>
2023-07-09 12:53:10 -0600

    Funcionamiento de boton next

--- Day2/index.js
+++ Day2/index.js
@@ -1,10 +1,19 @@
 const nextBtn = document.getElementById("next")
 const prevBtn = document.getElementById("prev")
 const btns = document.querySelectorAll(".btn")
+const nums = document.querySelectorAll(".num")
+const lines = document.querySelectorAll(".line")
+let i = 0
 
 nextBtn.addEventListener("click", () => {
-    console.log("next")
-    console.log(btns)
+    lines[i].style.backgroundColor = "var(--active)"
+    nums[i+1].style.borderColor = "var(--active)"
+    if(i >= 2) {
+        nextBtn.style.backgroundColor = "var(--desactive)"
+        nextBtn.style.cursor = "not-allowed"
+        return 
+    }
+    i += 1
 })
 
 prevBtn.addEventListener("click", () => {
