912706bd009065d3d089cc6e247aea0260842db4
Billy Torres <btorresr75@gmail.com>
2023-07-10 18:56:49 -0600

    Agregando funcionalidad

--- Day2/index.js
+++ Day2/index.js
@@ -8,14 +8,27 @@ let i = 0
 nextBtn.addEventListener("click", () => {
     lines[i].style.backgroundColor = "var(--active)"
     nums[i+1].style.borderColor = "var(--active)"
+    prevBtn.style.backgroundColor = "var(--active)"
     if(i >= 2) {
         nextBtn.style.backgroundColor = "var(--desactive)"
         nextBtn.style.cursor = "not-allowed"
         return 
     }
     i += 1
+    console.log(i)
 })
 
 prevBtn.addEventListener("click", () => {
-    console.log("prev")
+    nextBtn.style.backgroundColor = "var(--active)"
+    lines[i].style.backgroundColor = "var(--desactive)"
+    nums[i+1].style.borderColor = "var(--desactive)"
+    if(i < 0) {
+        prevBtn.style.backgroundColor = "var(--desactive)"
+        prevBtn.style.cursor = "not-allowed"
+        return 
+    }
+    i -= 1
+    console.log(i)
 })
+
+//TODO cambiar el HTML para enlazar numeros con lineas.
