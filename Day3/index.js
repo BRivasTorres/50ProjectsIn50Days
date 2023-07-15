d1a69b28aac2972e28a3fa2cfe4a1612209ed3b6
Billy Torres <btorresr75@gmail.com>
2023-07-15 17:53:12 -0600

    Agragando funcionalidad con JS

---
+++ Day3/index.js
@@ -0,0 +1,6 @@
+let btnMenu = document.querySelector(".menu")
+let content = document.querySelector(".content")
+
+btnMenu.addEventListener("click", () => {
+    content.className = "rotate"    
+})
