66e10073b94e88fd3bddd7be0bd2e99f48f17a69
Billy Torres <btorresr75@gmail.com>
2023-07-16 17:52:09 -0600

    Cambios en la funcionalidad del menu

--- Day3/index.js
+++ Day3/index.js
@@ -1,6 +1,23 @@
 let btnMenu = document.querySelector(".menu")
-let content = document.querySelector(".content")
+let main = document.querySelector(".main")
+let icons = document.querySelectorAll(".icons")
+let btnClose = document.querySelector(".close")
 
 btnMenu.addEventListener("click", () => {
-    content.className = "rotate"    
+    main.className = "rotate"
+    btnClose.classList.remove("hidden")    
+    btnMenu.classList = "hidden"
+})
+
+btn.addEventListener("click", () => {
+    icons.forEach(icon => {
+        icon.className.remove("invisible")
+    })
+})
+
+btnClose.addEventListener("click", () => {
+    btnClose.className = "hidden"
+    btnMenu.className.remove("hidden")
+    main.className.remove("rotate")
+    console.log("click")
 })
