e7e36e6ed7cdc9864c719b7a3f7eb712f393758f
Billy Torres <btorresr75@gmail.com>
2023-07-29 15:54:30 -0600

    Cambios en el maquetado del proyecto

---
+++ Day11/index.js
@@ -0,0 +1,11 @@
+let initialText = document.querySelector(".initialText")
+
+
+window.addEventListener("keydown", (e) => {
+    
+    console.log(e.key)
+    console.log(e.keyCode)
+    console.log(e.code)
+})
+
+//TODO: Agregar el texto a cada div 
