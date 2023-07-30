57c35fb2a4df782ffe812d9ab75b7d96199832e7
Billy Torres <btorresr75@gmail.com>
2023-07-30 09:25:55 -0600

    Proyecto del dia 11 finalizado2

--- Day11/index.js
+++ Day11/index.js
@@ -1,11 +1,15 @@
 let initialText = document.querySelector(".initialText")
-
+let keyEvents = document.querySelectorAll(".event")
+let eventKey = document.querySelector(".eventKey")
+let eventKeyCode = document.querySelector(".eventKeyCode")
+let eventCode = document.querySelector(".eventCode")
 
 window.addEventListener("keydown", (e) => {
-    
-    console.log(e.key)
-    console.log(e.keyCode)
-    console.log(e.code)
+    initialText.classList.add("invisible")
+    keyEvents.forEach(event => {
+        event.classList.remove("invisible")
+    })    
+    eventKey.innerHTML = e.key
+    eventKeyCode.innerHTML = e.keyCode
+    eventCode.innerHTML = e.code
 })
-
-//TODO: Agregar el texto a cada div 
