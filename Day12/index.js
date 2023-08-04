c31576943a5a9993d91ed56539ef3523a68852fc
Billy Torres <btorresr75@gmail.com>
2023-08-03 20:02:16 -0600

    Finalizacion de proyecto dia 12

--- Day12/index.js
+++ Day12/index.js
@@ -1,13 +1,14 @@
 let expandBtn = document.querySelectorAll(".expand")
 let closeBtn = document.querySelectorAll(".close")
 let answerText = document.querySelectorAll(".answer")
-
+let cuestionsWrapper = document.querySelectorAll(".cuestion")
 
 closeBtn.forEach((ele,i) => {
     ele.addEventListener("click", () => {
         ele.classList.add("desactive")
         expandBtn[i].classList.remove("desactive")
         answerText[i].classList.add("desactive")
+        cuestionsWrapper[i].style.backgroundColor = "transparent"
     })
 })
 
@@ -16,5 +17,6 @@ expandBtn.forEach((ele, i) => {
         ele.classList.add("desactive")
         closeBtn[i].classList.remove("desactive")
         answerText[i].classList.remove("desactive")
+        cuestionsWrapper[i].style.backgroundColor = "#fff"
     })
 })
