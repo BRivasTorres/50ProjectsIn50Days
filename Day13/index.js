6590410067e938dca824070834f8aaf720618489
Billy Torres <btorresr75@gmail.com>
2023-08-06 10:25:22 -0600

    Cambios en la funcionalidad del TextArea

--- Day13/index.js
+++ Day13/index.js
@@ -1,6 +1,21 @@
 let textArea = document.getElementById("choices-text")
+let tags = document.getElementById("tags")
 
 textArea.addEventListener("input", (e) => {
-    let textValue = textArea.value
-    console.log(textValue.split(""))
+    tags.innerHTML = ""
+    
+    const tagsArray = textArea.value.split(",")
+    
+    tagsArray.forEach(tagText => {
+        const tag = document.createElement("span")
+        tag.textContent = tagText.trim()
+        tags.appendChild(tag)
+    });
+    
+})
+
+textArea.addEventListener("keydown", (e) => {
+    if(e.keyCode === 13) {
+        textArea.value = ""
+    }
 })
