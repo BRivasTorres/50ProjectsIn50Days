ab3ca5d1fb6b8f55fd18bc7b80c52c6ab81cc6a3
Billy Torres <btorresr75@gmail.com>
2023-08-10 18:45:44 -0600

    Creación de funcion para generar el efecto random

--- Day13/index.js
+++ Day13/index.js
@@ -5,14 +5,18 @@ textArea.addEventListener("input", (e) => {
     tags.innerHTML = ""
     
     const tagsArray = textArea.value.split(",")
+    let max = tagsArray.length
     
     tagsArray.forEach(tagText => {
         const tag = document.createElement("span")
         tag.textContent = tagText.trim()
         tags.appendChild(tag)
-    });
-    
+    });    
 })
+
+const aleatory = () => {
+    let randomCurr = Math.round(Math.random() * max)
+}
 
 textArea.addEventListener("keydown", (e) => {
     if(e.keyCode === 13) {
