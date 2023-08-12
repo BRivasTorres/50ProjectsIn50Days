daf6490fa85e289c6b73a88f178bf8150e8f9fc1
Billy Torres <btorresr75@gmail.com>
2023-08-12 17:52:42 -0600

    Proyecto del dia 13 finalizado

--- Day13/index.js
+++ Day13/index.js
@@ -1,6 +1,8 @@
 let textArea = document.getElementById("choices-text")
 let tags = document.getElementById("tags")
 
+textArea.focus()
+
 textArea.addEventListener("input", (e) => {
     tags.innerHTML = ""
     
@@ -9,17 +11,61 @@ textArea.addEventListener("input", (e) => {
     
     tagsArray.forEach(tagText => {
         const tag = document.createElement("span")
+        tag.classList.add("tag")
         tag.textContent = tagText.trim()
         tags.appendChild(tag)
     });    
 })
 
-const aleatory = () => {
-    let randomCurr = Math.round(Math.random() * max)
-}
-
 textArea.addEventListener("keydown", (e) => {
     if(e.keyCode === 13) {
-        textArea.value = ""
+        setTimeout(() => {
+            e.target.value = ""
+        }, 10);
+            
+        randomSelect()
     }
 })
+
+function randomSelect() {    
+    const times = 30
+    
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+        
+        if(randomTag !== null) {
+            higlightTag(randomTag)
+        }
+        
+        setTimeout(() => {
+            unHiglightTag(randomTag)
+        }, 100);
+    }, 100);
+    
+    setTimeout(() => {
+        clearInterval(interval)
+        
+        setTimeout(() => {
+            setTimeout(() => {
+                const randomTag = pickRandomTag()
+                higlightTag(randomTag)
+            }, 100);
+        }, 100);
+    }, times * 100);
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll(".tag")
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+
+function higlightTag(tag) {
+    tag.classList.add("curr-choice")
+    console.log(tag)
+}
+
+function unHiglightTag(tag) {
+    tag.classList.remove("curr-choice")
+    console.log(tag)
+}
