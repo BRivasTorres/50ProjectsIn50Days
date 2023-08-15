5657bdfb4f29f392ce95f1de1432ab758d3c8e3c
Billy Torres <btorresr75@gmail.com>
2023-08-15 09:08:43 -0600

    Dia 14 finalizado

--- Day14/index.js
+++ Day14/index.js
@@ -1,7 +1,12 @@
 let openClose = document.querySelector(".open-close") 
 let line1 = document.querySelector(".line1")
 let line2 = document.querySelector(".line2")
+let li = document.querySelectorAll(".li")
+let nav = document.querySelector(".nav")
 
 openClose.addEventListener("click", () => {
-    line1.classList.toggle("close")
+    li.forEach(li => {
+        li.classList.toggle("active")
+    })
+    nav.classList.toggle("active")
 })
