54ac4680ddff16e8019ef8515cefef23378f14ce
Billy Torres <btorresr75@gmail.com>
2023-08-17 09:17:53 -0600

    %1

--- Day14/index.js
+++ Day14/index.js
@@ -1,12 +1,12 @@
-let openClose = document.querySelector(".open-close") 
-let line1 = document.querySelector(".line1")
-let line2 = document.querySelector(".line2")
-let li = document.querySelectorAll(".li")
-let nav = document.querySelector(".nav")
+let openClose = document.querySelector(".open-close");
+let line1 = document.querySelector(".line1");
+let line2 = document.querySelector(".line2");
+let li = document.querySelectorAll(".li");
+let nav = document.querySelector(".nav");
 
 openClose.addEventListener("click", () => {
-    li.forEach(li => {
-        li.classList.toggle("active")
-    })
-    nav.classList.toggle("active")
-})
+	li.forEach((li) => {
+		li.classList.toggle("active");
+	});
+	nav.classList.toggle("active");
+});
