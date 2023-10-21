7e30f889e1a945979aa6a7ad30b8406b282f6576
Billy Torres <btorresr75@gmail.com>
2023-10-21 09:28:37 -0600

    feat : cambios en la funcionalidad

--- Day21/index.js
+++ Day21/index.js
@@ -1,5 +1,39 @@
-let img = document.getElementById("img");
+const img = document.querySelector(".img");
+const boxes = document.querySelectorAll(".boxes");
 
-img.addEventListener("mouseover", () => {
-	console.log("hi");
-});
+img.addEventListener("dragstart", dragStart);
+img.addEventListener("dragend", dragEnd);
+
+for (const box of boxes) {
+	box.addEventListener("dragover", dragOver);
+	box.addEventListener("dragenter", dragEnter);
+	box.addEventListener("dragleave", dragLeave);
+	box.addEventListener("drop", dragDrop);
+}
+
+function dragStart() {
+	this.className += " hold";
+	setTimeout(() => (this.className = "invisible"), 0);
+}
+
+function dragEnd() {
+	this.className = "img";
+}
+
+function dragOver(e) {
+	e.preventDefault();
+}
+
+function dragEnter(e) {
+	e.preventDefault();
+	this.className += " hovered";
+}
+
+function dragLeave() {
+	this.className = "boxes";
+}
+
+function dragDrop() {
+	this.className = "boxes";
+	this.append(img);
+}
