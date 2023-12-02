c938bff0cd687ee27c065034b9882fdbfd350ae0
Billy Torres <btorresr75@gmail.com>
2023-12-02 11:06:18 -0600

    dia 29 finalizado

--- Day29/index.js
+++ Day29/index.js
@@ -1,8 +1,31 @@
-const btnImg = document.querySelector(".main__btn");
+const divImg = document.querySelector(".main__div");
 const clickedTimes = document.querySelector(".main__h4--span");
 
-btnImg.addEventListener("click", () => {
+divImg.addEventListener("click", (e) => {
 	count = parseInt(clickedTimes.innerHTML);
 	newCount = count + 1;
-	console.log(newCount);
+	clickedTimes.innerHTML = `${newCount}`;
+	createHeart(e);
 });
+
+const createHeart = (e) => {
+	const heart = document.createElement("i");
+	heart.classList.add("fas");
+	heart.classList.add("fa-heart");
+
+	const x = e.clientX;
+	const y = e.clientY;
+
+	const leftOffset = e.target.offsetLeft;
+	const topOffset = e.target.offsetTop;
+
+	const xInside = x - leftOffset;
+	const yInside = y - topOffset;
+
+	heart.style.top = `${yInside}px`;
+	heart.style.left = `${xInside}px`;
+
+	divImg.appendChild(heart);
+
+	setTimeout(() => heart.remove(), 1000);
+};
