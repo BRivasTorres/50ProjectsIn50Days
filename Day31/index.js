63f7baa52c93257f28c209434b7a7bbf8f5e7e49
Billy Torres <btorresr75@gmail.com>
2023-12-10 07:47:21 -0600

    proyecto dia 31 finalizado

--- Day31/index.js
+++ Day31/index.js
@@ -1,4 +1,5 @@
 const generateBtn = document.querySelector(".main__btn")
+const copyBtn = document.querySelector(".main__button--copy")
 const inputLength = document.querySelector("#length")
 const inputUppercase = document.querySelector("#uppercase")
 const inputLowercase = document.querySelector("#lowercase")
@@ -12,31 +13,45 @@ const numbers = Array.from({ length: 10 }, (_, i) => i.toString());
 const symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', ';', ':', ',', '.', '<', '>', '/', '?'];
 
 let res = ""
-let chars = [];    
-let passwordLength = inputLength.value
+let chars = [];
 
-const handleLength = () => {
+generateBtn.addEventListener("click", () => { 
+    let passwordLength = inputLength.value;
     inputLength.addEventListener("click", (e) => {
-		passwordLength = e.target.value;
-	});
-}
-
-generateBtn.addEventListener("click", () => {
+        passwordLength = e.target.value;
+    });
+    
     res = "";
     if (inputLowercase.checked) chars.push(...lowercaseLetters);
     if (inputUppercase.checked) chars.push(...uppercaseLetters);
     if (inputSymbols.checked) chars.push(...symbols);
     if (inputNumbers.checked) chars.push(...numbers);
-    handleLength()
     generatePassword(chars, passwordLength);
+    printPassword(res)
 });
 
 const generatePassword = (arr, length) => {
+    if(arr.length === 0) return
     for(let i = 0; i < length; i++) {
         let j = Math.floor(Math.random() * arr.length)
         res += arr[j]
     }
     chars = [];
-    console.log(length)
 }
 
+const printPassword = (pass) => {
+    if(span.innerHTML.length > 0) {
+        span.innerHTML = ""
+    }    
+    span.innerHTML = `${pass}`;
+}
+
+const copyPassword = () => {
+    if(!res) return
+    let pass = span.innerText
+    navigator.clipboard.writeText(pass)
+        .then(() => alert("Password copied to clipboard "))
+        .catch(err => alert("Error " + err))   
+}
+
+copyBtn.addEventListener("click", copyPassword);
