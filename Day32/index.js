051e25f5fb12c39a38b7738dcf82f44f88986fad
Billy Torres <btorresr75@gmail.com>
2023-12-25 09:47:23 -0600

    day 32 finished

--- Day32/index.js
+++ Day32/index.js
@@ -2,12 +2,10 @@ const btns = document.querySelectorAll(".main__button")
 const circles = document.querySelectorAll(".circle")
 
 btns.forEach((btn, i) => {
-    let currActives = 0
+    let curr = circles[i].classList
     
     btn.addEventListener("click", () => {
-        btn.classList.toggle("active")
-        let curr = circles[i].classList
-                
+        btn.classList.toggle("active");
         if (curr.contains("activeAnimation")) {
             curr.remove("activeAnimation");
             curr.add("desactiveAnimation");
@@ -15,6 +13,26 @@ btns.forEach((btn, i) => {
             curr.remove("desactiveAnimation");
             curr.add("activeAnimation");
         }
-        currActives++
+        trick(i)
     })
 })
+
+const trick = (index) => {
+    if([...btns].every(el => el.classList.contains("active"))) {
+        {
+			if (index === 2) {
+				circles[1].classList.remove("activeAnimation");
+				circles[1].classList.add("desactiveAnimation");
+				btns[1].classList.remove("active");
+			} else if (index === 1) {
+				circles[0].classList.remove("activeAnimation");
+				circles[0].classList.add("desactiveAnimation");
+				btns[0].classList.remove("active");
+			} else {
+				circles[2].classList.remove("activeAnimation");
+				circles[2].classList.add("desactiveAnimation");
+				btns[2].classList.remove("active");
+			}
+		}
+    } 
+}
