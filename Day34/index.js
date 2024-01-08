4ba91cffaa983a1d8f6da996f61a722bc1beb87d
Billy Torres <btorresr75@gmail.com>
2024-01-08 10:59:28 -0600

    feat : animation functionality with button click

--- Day34/index.js
+++ Day34/index.js
@@ -1,5 +1,41 @@
 const btn = document.querySelector(".main__button")
+const div = document.querySelector(".animated__div")
+const num = document.querySelector(".animated__num")
+const mainCont = document.querySelector(".main__content")
+
+let countID;
+const updateCount = () => {
+    countID = setInterval(() => {
+		let temp = parseInt(num.innerHTML);
+		let newTemp = temp - 1;
+		num.innerHTML = newTemp;
+
+		if (newTemp < 0) {
+			num.innerHTML = 3;
+		}
+	}, 1000);
+}
+
+window.addEventListener("load", () => {
+    num.classList.add("animation-num")
+    updateCount()
+    
+    setTimeout(() => {
+        div.style.display = "none"
+        mainCont.style.display = "block"
+        clearInterval(countID);
+    }, 4000);
+})
 
 btn.addEventListener("click", () => {
+    mainCont.style.display = "none"
+    div.style.display = "block"
+    num.classList.add("animation-num")
+    updateCount();    
     
+    setTimeout(() => {
+        div.style.display = "none"
+        mainCont.style.display = "block"
+        clearInterval(countID);
+    }, 4000)
 })
