b3d14a258c63bfc5dcff1f213f95e0a17cbc8d7f
Billy Torres <btorresr75@gmail.com>
2024-02-17 09:56:37 -0600

    feat : Finish proyect day 43

--- Day43/index.js
+++ Day43/index.js
@@ -1,21 +1,28 @@
-const serch = document.querySelector(".form__search")
 const form = document.querySelector(".main__form")
+const serchInput = document.querySelector(".form__search")
 const dataWrapper = document.querySelector(".main__data")
 const main = document.querySelector(".main")
 
+let dataArr = []
+
 window.addEventListener("load", () => {
-    getData()
+    getData("https://randomuser.me/api?results=50");
 })
-
 form.addEventListener("submit", (e) => e.preventDefault())
-serch.addEventListener("input", () => {
+
+
+serchInput.addEventListener("input", (e) => {
+    const val = e.target.value
+    const newData = dataArr.filter(item => item.name.first.toLowerCase().includes(val) || item.location.country.toLowerCase().includes(val))
+    printData(newData)
 })
 
-const getData = async() => {
+const getData = async(url) => {
     loadingData()
-    const response = await fetch(` https://randomuser.me/api?results=50`);
+    const response = await fetch(`${url}`);
     const data = await response.json()
-    return printData(data.results)
+    dataArr = data.results
+    return printData(dataArr)
 }
 
 const printData = (data) => {
@@ -23,24 +30,33 @@ const printData = (data) => {
     const dataSection = document.createElement("section")
     dataSection.classList.add("data__wrapper")
     
-    for(let i = 0; i < data.length; i++) {
-        const card = document.createElement("div")
-        card.classList.add("wrapper__card")
-
-        const name = data[i].name.first + data[i].name.last
-        const imgSrc = data[i].picture.thumbnail
-        const location = data[i].location.state + data[i].location.country
-        
-        card.innerHTML = `
-            <img src=${imgSrc} alt=${name} class="card__img" />
-            <aside class="card__aside">
-                <h3 class="card__name">${name}</h3>
-                <span class="card__location">${location}</span>
-            </aside>
-        `;   
-        dataSection.appendChild(card)
+    if(data.length > 0) {
+        for(let i = 0; i < data.length; i++) {
+            const card = document.createElement("div")
+            card.classList.add("wrapper__card")
+    
+            const name = data[i].name.first + " " + data[i].name.last
+            const imgSrc = data[i].picture.thumbnail
+            const location = data[i].location.state + " " + data[i].location.country
+            
+            card.innerHTML = `
+                <img src=${imgSrc} alt=${name} class="card__img" />
+                <aside class="card__aside">
+                    <h3 class="card__name">${name}</h3>
+                    <span class="card__location">${location}</span>
+                </aside>
+            `;   
+            dataSection.appendChild(card)
+        }
+        dataWrapper.appendChild(dataSection);
+    } else {
+        const h2 = document.createElement("h2")
+        h2.style.padding = "2rem"
+        h2.style.fontSize = "2.2rem"
+        h2.innerHTML = "No data found..."
+        dataWrapper.appendChild(h2)
     }
-    dataWrapper.appendChild(dataSection);
+    
 }
 
 const loadingData = () => {
