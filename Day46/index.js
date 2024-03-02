c3a6139dff9e1c8b31dc2f43e3d5c0381083e2d6
Billy Torres <btorresr75@gmail.com>
2024-03-01 22:05:46 -0800

    feat : Init proyect day 46

---
+++ Day46/index.js
@@ -0,0 +1,41 @@
+const main = document.querySelector(".main")
+
+const questions = [
+	{
+		question: "Which language runs in a web browser?",
+		options: ["Java", "C", "Phyton", "JavaScript"],
+	},
+	{
+		question: "What does CSS stand for?",
+		options: [
+			"Central Style Sheets",
+			"Cascading Style Sheets",
+			"Cascading Simple Sheets",
+			"Cars SUVs Sailboats",
+		],
+	},
+	{
+		question: "What does HTML stand for?",
+		options: [
+			"Hypertext Markup Language",
+			"Hypertext Markdown Language",
+			"Hyperloop Machine Language",
+			" Helicopters Terminals Motorboats Lamborginis",
+		],
+	},
+	{
+		question: "What year was JavaScript launched?",
+		options: [1996, 1994, 1996, "none of the above"],
+	},
+];
+
+const printForm = (data, id) => {
+    const div = document.createElement("div")
+}
+
+// const formQuestions = {
+// 	firstForm: {
+// 		question: "Which language runs in a web browser?",
+//         options: ["Java", "C", "Phyton", "JavaScript"]
+// 	},
+// };
