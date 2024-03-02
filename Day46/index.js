3be311e79d113b1c1b64bb1f26cdc734a93cd161
Billy Torres <btorresr75@gmail.com>
2024-03-02 13:50:57 -0800

    feat : Create data from the forms

--- Day46/index.js
+++ Day46/index.js
@@ -1,41 +1,57 @@
 const main = document.querySelector(".main")
-
-const questions = [
-	{
-		question: "Which language runs in a web browser?",
-		options: ["Java", "C", "Phyton", "JavaScript"],
-	},
-	{
-		question: "What does CSS stand for?",
-		options: [
-			"Central Style Sheets",
-			"Cascading Style Sheets",
-			"Cascading Simple Sheets",
-			"Cars SUVs Sailboats",
-		],
-	},
-	{
-		question: "What does HTML stand for?",
-		options: [
-			"Hypertext Markup Language",
-			"Hypertext Markdown Language",
-			"Hyperloop Machine Language",
-			" Helicopters Terminals Motorboats Lamborginis",
-		],
-	},
-	{
-		question: "What year was JavaScript launched?",
-		options: [1996, 1994, 1996, "none of the above"],
-	},
-];
-
-const printForm = (data, id) => {
-    const div = document.createElement("div")
+function printForm() {
+	const questions = [
+		{
+			question: "Which language runs in a web browser?",
+			options: ["Java", "C", "Phyton", "JavaScript"],
+		},
+		{
+			question: "What does CSS stand for?",
+			options: [
+				"Central Style Sheets",
+				"Cascading Style Sheets",
+				"Cascading Simple Sheets",
+				"Cars SUVs Sailboats",
+			],
+		},
+		{
+			question: "What does HTML stand for?",
+			options: [
+				"Hypertext Markup Language",
+				"Hypertext Markdown Language",
+				"Hyperloop Machine Language",
+				" Helicopters Terminals Motorboats Lamborginis",
+			],
+		},
+		{
+			question: "What year was JavaScript launched?",
+			options: [1996, 1994, 1996, "none of the above"],
+		},
+	];
+	
+	for(let i = 0; i < 1; i++) {
+		const form = document.createElement("form")
+		form.classList.add("main__form")
+		form.innerHTML = `
+			<section>
+				<h2 class="form__title">${questions[i].question}</h2>
+				<div class="form__options">
+					${printOptionsForm(questions[i].options.length, i)}
+				</div>
+			</section>
+			<button type="submit" class="form__submit">Submit</button>
+		`;
+		main.appendChild(form)
+	}
+	
+	function printOptionsForm (length, currentEl) {
+		const optionsWrapper = document.createElement("div")
+		for(let i = 0; i <= length; i++) {
+			const currentOption = questions[currentEl].options[i]
+			optionsWrapper.innerHTML += `<label for="options${i}">${currentOption}</label>`
+			optionsWrapper.innerHTML += `<input type="radio" name="options${i}" id="options${i}" />`;
+			return optionsWrapper
+		}
+	}
 }
-
-// const formQuestions = {
-// 	firstForm: {
-// 		question: "Which language runs in a web browser?",
-//         options: ["Java", "C", "Phyton", "JavaScript"]
-// 	},
-// };
+printForm();
